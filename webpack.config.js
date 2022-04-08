9b92f5eb820b1e1e2322d0b0bd8f0d77b7988142
Walany Costa <walanyBnegro@gmail.com>
2022-04-08 22:37:09 +0100

    fix: solve the error ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: './src/main/index.tsx',
   output: {
-    path: path.join(__dirname,'public/js'),
+    path: path.resolve(__dirname,'public/js'),
     publicPath: '/public/js',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    clean: true
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      '@': path.join(__dirname, 'src')
+      '@': path.resolve(__dirname, 'src')
     }
   },
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    },{
-      test: /\.scss$/,
-      use: [{
+    rules: [
+      {
+        test: /\.(jsx?|tsx?)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },{
+        test: /\.scss$/,
+        use: [{
         loader: 'style-loader'
       },{
         loader: 'css-loader',
@@ -30,20 +31,20 @@ module.exports = {
           modules: true
         }
       },{
-        loader: 'sass-loader'
+        loader: 'sass-loader',
+        options:{
+          implementation: require('sass')
+        }
       }]
     }]
   },
+  devtool:'inline-source-map',
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
+    static: './public',
+    compress: true,
+    hot: true,
+    open: true,
+    port: 3000,
     historyApiFallback: true
-  },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDom'
-  },
-  plugins: [
-    new CleanWebpackPlugin()
-  ]
+  }
 }
