5987a67af0c991dc1dc243a55efa9ac4bee1692c
Walany Costa <walanyBnegro@gmail.com>
2022-04-16 02:52:49 +0100

    feat: ensure Login present error if email is valid

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -20,7 +20,10 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   });
 
   useEffect(() => {
-    validation.validate('email', state.email);
+    setState({
+      ...state,
+      emailError: validation.validate('email', state.email)
+    });
   }, [state.email]);
 
   useEffect(() => {
