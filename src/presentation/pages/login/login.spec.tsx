7d683b4dfde6aaa82705a0d3c5c4c9766ef9c2fa
Walany Costa <walanyBnegro@gmail.com>
2022-04-16 07:19:00 +0100

    test: ensure Login present valid state if password is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -52,4 +52,14 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe(validationStub.errorMessage);
     expect(passwordStatus.textContent).toBe('🔴');
   });
+
+  test('should show valid password state if validation succeeds', () => {
+    const { sut, validationStub } = makeSut();
+    const passwordInput = sut.getByTestId('password');
+    validationStub.errorMessage = null;
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } });
+    const passwordStatus = sut.getByTestId('password-status');
+    expect(passwordStatus.title).toBe('tudo certo');
+    expect(passwordStatus.textContent).toBe('🟢');
+  });
 });
