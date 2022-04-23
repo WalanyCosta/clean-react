57f9ea941431430274349bff8c6deb6c732323f8
Walany Costa <walanyBnegro@gmail.com>
2022-04-23 04:03:14 +0100

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { InvalidFieldError } from '@/validation/errors';
+import faker from '@faker-js/faker';
 import { EmailValidation } from './email-validation';
 
 describe('RequiredFieldValidation', () => {
   test('should return error if field is empty', () => {
-    const sut = new EmailValidation('email');
+    const sut = new EmailValidation(faker.random.word());
     const error = sut.validate('');
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test('should return falsy if email is valid', () => {
+    const sut = new EmailValidation('email');
+    const error = sut.validate(faker.internet.email());
+    expect(error).toBeFalsy();
   });
 });
