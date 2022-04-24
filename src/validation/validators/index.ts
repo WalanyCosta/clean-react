cecc84a15d2d5678bc4495def65a6cbf0c978364
Walany Costa <walanyBnegro@gmail.com>
2022-04-24 19:41:27 +0100

    refactor: change ValidationComposite to use a static method to create instance

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -1,3 +1,5 @@
 export * from './email/email-validation';
 export * from './required-field/required-field-validation';
 export * from './min-length/min-length-validation';
+export * from './builder/validation-builder';
+export * from './validation-composite/validation-composite';
