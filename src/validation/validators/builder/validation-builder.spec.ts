df658135eba10e1f94a9a67fd3063d1b66c5be1e
Walany Costa <walanyBnegro@gmail.com>
2022-04-24 19:03:10 +0100

    test: ensure ValidationBuilder has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,9 +1,15 @@
 import { RequiredFieldValidation } from '@/validation/validators';
+import { EmailValidation } from '../email/email-validation';
 import { ValidationBuilder } from './validation-builder';
 
 describe('ValidationBuilder', () => {
   test('should returns required-field', () => {
     const validations = ValidationBuilder.field('any_field').required().build();
     expect(validations).toEqual([new RequiredFieldValidation('any_field')]);
+  });
+
+  test('should returns email-field', () => {
+    const validations = ValidationBuilder.field('any_field').email().build();
+    expect(validations).toEqual([new EmailValidation('any_field')]);
   });
 });
