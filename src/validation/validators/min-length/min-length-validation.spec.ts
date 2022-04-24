cad471ac08a70a1b31fc2775202998c08e950202
Walany Costa <walanyBnegro@gmail.com>
2022-04-24 13:19:30 +0100

    test: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -3,8 +3,14 @@ import { MinLengthValidation } from './min-length-validation';
 
 describe('MinLengthValidation', () => {
   test('Should return error if value is invalid', () => {
-    const sut = new MinLengthValidation('password', 5);
+    const sut = new MinLengthValidation('field', 5);
     const error = sut.validate('123');
     expect(error).toEqual(new InvalidFieldError());
+  });
+
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation('field', 5);
+    const error = sut.validate('123456');
+    expect(error).toBeFalsy();
   });
 });
