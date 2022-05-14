06ae79b79f966a36efeec64a7ab012bf1ca0b0cf
Walany Costa <walanyBnegro@gmail.com>
2022-05-14 17:48:24 +0100

    chore: add firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBWveynfNg9WQOiy9bAZkE3YUojva7q_kM",
+  authDomain: "fordev-ccd2a.firebaseapp.com",
+  projectId: "fordev-ccd2a",
+  storageBucket: "fordev-ccd2a.appspot.com",
+  messagingSenderId: "111139295973",
+  appId: "1:111139295973:web:fd8c0aa60f7517f18dba0c",
+  measurementId: "G-LN8HPEDB2R"
+};
+
+
