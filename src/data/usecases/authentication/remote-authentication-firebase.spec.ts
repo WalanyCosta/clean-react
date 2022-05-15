f6c7dd27b3559a2002881ecab9b416618d403f14
Walany Costa <walanyBnegro@gmail.com>
2022-05-15 12:55:49 +0100

    refactor: move SUT creation to a factory method

--- src/data/usecases/authentication/remote-authentication-firebase.spec.ts
+++ src/data/usecases/authentication/remote-authentication-firebase.spec.ts
@@ -5,25 +5,38 @@ import { AccountModel } from '@/domain/model';
 import { mockAuthentication } from '@/domain/test';
 import { RemoteAuthenticationFirebase } from './remote-authentication-firebase';
 
+type SutTypes ={
+  sut: RemoteAuthenticationFirebase
+  authWithEmailAndPasswordSpy: AuthWithEmailAndPasswordSpy<AccountModel>;
+}
+
+const makeSut = (): SutTypes => {
+  const authWithEmailAndPasswordSpy = new AuthWithEmailAndPasswordSpy<AccountModel>();
+  const sut = new RemoteAuthenticationFirebase(authWithEmailAndPasswordSpy);
+  authWithEmailAndPasswordSpy.response = {
+    statusCode: StatusCode.ok
+  };
+
+  return {
+    sut,
+    authWithEmailAndPasswordSpy
+  };
+};
+
 describe('RemoteAuthenticationFirebase', () => {
   test('should call authWithEmailAndPasswordSpy with correct params', async () => {
-    const authWithEmailAndPasswordSpy = new AuthWithEmailAndPasswordSpy<AccountModel>();
-    const remoteAuthenticationFirebase = new RemoteAuthenticationFirebase(authWithEmailAndPasswordSpy);
+    const { sut, authWithEmailAndPasswordSpy } = makeSut();
     const authentication = mockAuthentication();
-    authWithEmailAndPasswordSpy.response = {
-      statusCode: StatusCode.ok
-    };
-    await remoteAuthenticationFirebase.auth(authentication);
+    await sut.auth(authentication);
     expect(authWithEmailAndPasswordSpy.params).toBe(authentication);
   });
 
   test('should throw InvalidCredentialsError AuthFirebase returns 401 ', async () => {
-    const authWithEmailAndPasswordSpy = new AuthWithEmailAndPasswordSpy<AccountModel>();
-    const remoteAuthenticationFirebase = new RemoteAuthenticationFirebase(authWithEmailAndPasswordSpy);
+    const { sut, authWithEmailAndPasswordSpy } = makeSut();
     authWithEmailAndPasswordSpy.response = {
       statusCode: StatusCode.unauthorized
     };
-    const promise = remoteAuthenticationFirebase.auth(mockAuthentication());
+    const promise = sut.auth(mockAuthentication());
     await expect(promise).rejects.toThrow(new InvalidCredentialsError());
   });
 });
