bd4d33e2a98bada305a48a4e39b9d6a7aab79ae5
Walany Costa <walanyBnegro@gmail.com>
2022-05-15 18:21:00 +0100

    refactor: move SUT creation to a factory method

--- src/infra/firebase/authentication/auth-with-email-and-password.spec.ts
+++ src/infra/firebase/authentication/auth-with-email-and-password.spec.ts
@@ -4,13 +4,27 @@ import { AuthWithEmailAndPassword } from './auth-with-email-and-password';
 
 jest.mock('firebase/auth');
 
+type SutTypes ={
+  sut: AuthWithEmailAndPassword;
+  mockedSignIn: jest.Mocked<typeof signInWithEmailAndPassword>;
+}
+
+const makeSut = (auth = getAuth()) : SutTypes => {
+  const mockedSignIn = signInWithEmailAndPassword as jest.Mocked<typeof signInWithEmailAndPassword>;
+  const sut = new AuthWithEmailAndPassword(auth);
+
+  return {
+    sut,
+    mockedSignIn
+  };
+};
+
 describe('AuthWithEmailAndPassword', () => {
   test('should calls signInWithEmailAndPassword with correct values', async () => {
     const auth = getAuth();
     const authParams = mockAuthentication();
-    const mockedSignIn = signInWithEmailAndPassword as jest.Mocked<typeof signInWithEmailAndPassword>;
-    const authWithEmailAndPassword = new AuthWithEmailAndPassword(auth);
-    await authWithEmailAndPassword.authFirebase(authParams);
+    const { sut, mockedSignIn } = makeSut(auth);
+    await sut.authFirebase(authParams);
     expect(mockedSignIn).toHaveBeenCalledWith(auth, authParams.email, authParams.password);
   });
 });
