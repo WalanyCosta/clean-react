3375e98ae4ec65ca01ad52294f25011e9072ea24
Walany Costa <walanyBnegro@gmail.com>
2022-05-15 12:42:26 +0100

    test: ensure RemoteAuthenticationFirebase throws InvalidCredentialsError on 401

--- src/data/test/mock-auth-with-email-and-password.ts
+++ src/data/test/mock-auth-with-email-and-password.ts
@@ -1,11 +1,13 @@
 import { AuthenticationParams } from '@/domain/usecases';
 import { AuthFirebase } from '../protocols/firebase/auth-firebase';
+import { Response } from '@/data/protocols/firebase/response';
 
-export class AuthWithEmailAndPasswordSpy implements AuthFirebase {
+export class AuthWithEmailAndPasswordSpy<T> implements AuthFirebase<T> {
   params : AuthenticationParams;
+  response: Response<T>;
 
-  async authFirebase (params: AuthenticationParams): Promise<void> {
+  async authFirebase (params: AuthenticationParams): Promise<Response<T>> {
     this.params = params;
-    return Promise.resolve();
+    return this.response;
   }
 }
--- src/data/usecases/authentication/remote-authentication-firebase.spec.ts
+++ src/data/usecases/authentication/remote-authentication-firebase.spec.ts
@@ -1,13 +1,29 @@
+import { StatusCode } from '@/data/protocols/firebase/response';
 import { AuthWithEmailAndPasswordSpy } from '@/data/test/mock-auth-with-email-and-password';
+import { InvalidCredentialsError } from '@/domain/errors';
+import { AccountModel } from '@/domain/model';
 import { mockAuthentication } from '@/domain/test';
 import { RemoteAuthenticationFirebase } from './remote-authentication-firebase';
 
 describe('RemoteAuthenticationFirebase', () => {
-  test('should call authWithEmailAndPasswordSpy with correct params', () => {
-    const authWithEmailAndPasswordSpy = new AuthWithEmailAndPasswordSpy();
+  test('should call authWithEmailAndPasswordSpy with correct params', async () => {
+    const authWithEmailAndPasswordSpy = new AuthWithEmailAndPasswordSpy<AccountModel>();
     const remoteAuthenticationFirebase = new RemoteAuthenticationFirebase(authWithEmailAndPasswordSpy);
     const authentication = mockAuthentication();
-    remoteAuthenticationFirebase.auth(authentication);
+    authWithEmailAndPasswordSpy.response = {
+      statusCode: StatusCode.ok
+    };
+    await remoteAuthenticationFirebase.auth(authentication);
     expect(authWithEmailAndPasswordSpy.params).toBe(authentication);
+  });
+
+  test('should throw InvalidCredentialsError AuthFirebase returns 401 ', async () => {
+    const authWithEmailAndPasswordSpy = new AuthWithEmailAndPasswordSpy<AccountModel>();
+    const remoteAuthenticationFirebase = new RemoteAuthenticationFirebase(authWithEmailAndPasswordSpy);
+    authWithEmailAndPasswordSpy.response = {
+      statusCode: StatusCode.unauthorized
+    };
+    const promise = remoteAuthenticationFirebase.auth(mockAuthentication());
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError());
   });
 });
