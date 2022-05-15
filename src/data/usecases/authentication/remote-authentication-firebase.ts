38423b3b6cd9ca17bd80589e4b48656deeb3b179
Walany Costa <walanyBnegro@gmail.com>
2022-05-15 13:24:20 +0100

    feat: ensure RemoteAuthenticationFirebase throws UnexpectedError on 500

--- src/data/usecases/authentication/remote-authentication-firebase.ts
+++ src/data/usecases/authentication/remote-authentication-firebase.ts
@@ -1,6 +1,6 @@
 import { AuthFirebase } from '@/data/protocols/firebase/auth-firebase';
 import { StatusCode } from '@/data/protocols/firebase/response';
-import { InvalidCredentialsError } from '@/domain/errors';
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors';
 import { AccountModel } from '@/domain/model';
 import { Authentication, AuthenticationParams } from '@/domain/usecases';
 
@@ -11,6 +11,7 @@ export class RemoteAuthenticationFirebase implements Authentication {
     const response = await this.authFirebase.authFirebase(params);
     switch (response.statusCode) {
       case StatusCode.unauthorized: throw new InvalidCredentialsError();
+      case StatusCode.serverError: throw new UnexpectedError();
       default: return response.body;
     }
   }
