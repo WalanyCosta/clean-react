96163be8346959fbedd905fec1b7464fa122b8fa
Walany Costa <walanyBnegro@gmail.com>
2022-05-21 20:37:49 +0100

    feat: ensure AuthWithEmailAndPassword returns code

--- src/infra/firebase/authentication/auth-with-email-and-password.ts
+++ src/infra/firebase/authentication/auth-with-email-and-password.ts
@@ -4,10 +4,16 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 export class AuthWithEmailAndPassword implements AuthFirebase<any> {
   async authFirebase (param: AuthenticationParams): Promise<Response<any>> {
-    const response = await signInWithEmailAndPassword(getAuth(), param.email, param.password);
-    return {
-      statusCode: 200,
-      body: response.user
-    };
+    try {
+      const response = await signInWithEmailAndPassword(getAuth(), param.email, param.password);
+      return {
+        statusCode: 200,
+        body: response.user
+      };
+    } catch (erro) {
+      return {
+        statusCode: erro.code
+      };
+    }
   }
 }
