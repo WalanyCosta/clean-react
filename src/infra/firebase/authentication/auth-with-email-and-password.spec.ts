216341f9d907e774c7f0ab6c8aa84151cc3f57e1
Walany Costa <walanyBnegro@gmail.com>
2022-05-21 20:00:42 +0100

    test: ensure AuthWithEmailAndPassword returns corrent values

--- src/infra/firebase/authentication/auth-with-email-and-password.spec.ts
+++ src/infra/firebase/authentication/auth-with-email-and-password.spec.ts
@@ -1,30 +1,54 @@
 import { mockAuthentication } from '@/domain/test';
-import { Auth, getAuth, signInWithEmailAndPassword, User } from 'firebase/auth';
+import faker from '@faker-js/faker';
+import { getAuth, signInWithEmailAndPassword, User } from 'firebase/auth';
 import { AuthWithEmailAndPassword } from './auth-with-email-and-password';
+import { Response } from '@/data/protocols/firebase';
 
-jest.mock('firebase/auth');
+jest.mock('firebase/auth', () => {
+  return {
+    getAuth: jest.fn(),
+    signInWithEmailAndPassword: jest.fn()
+  };
+});
 
 type SutTypes ={
   sut: AuthWithEmailAndPassword;
-  mockedSignIn: jest.Mocked<typeof signInWithEmailAndPassword>;
+  mockedSignIn: jest.Mock<any, any>;
+  object: string | number;
 }
 
-const makeSut = (auth = getAuth()) : SutTypes => {
-  const mockedSignIn = signInWithEmailAndPassword as jest.Mocked<typeof signInWithEmailAndPassword>;
-  const sut = new AuthWithEmailAndPassword(auth);
+const makeSut = () : SutTypes => {
+  const mockedSignIn = signInWithEmailAndPassword as jest.Mock<any, any>;
+  const sut = new AuthWithEmailAndPassword();
+  const object = faker.random.objectElement({ keyA: 'may', keyB: 42 });
+  mockedSignIn.mockResolvedValue({
+    user: object
+  });
 
   return {
     sut,
-    mockedSignIn
+    mockedSignIn,
+    object
   };
 };
 
+const mockedResult = (object): Response<any> => ({
+  statusCode: 200,
+  body: object
+});
+
 describe('AuthWithEmailAndPassword', () => {
   test('should calls signInWithEmailAndPassword with correct values', async () => {
-    const auth = getAuth();
     const authParams = mockAuthentication();
-    const { sut, mockedSignIn } = makeSut(auth);
+    const { sut, mockedSignIn } = makeSut();
     await sut.authFirebase(authParams);
-    expect(mockedSignIn).toHaveBeenCalledWith(auth, authParams.email, authParams.password);
+    expect(mockedSignIn).toHaveBeenCalledWith(getAuth(), authParams.email, authParams.password);
+  });
+
+  test('should return the correct statusCode and body', async () => {
+    const authParams = mockAuthentication();
+    const { sut, object } = makeSut();
+    const promise = sut.authFirebase(authParams);
+    expect(promise).toEqual(Promise.resolve(mockedResult(object)));
   });
 });
