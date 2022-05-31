20038f90a8f372c5ca27fc37661cf48e1a78781d
Walany Costa <walanyBnegro@gmail.com>
2022-05-31 23:39:01 +0100

    feat: add env var to keys of firebase

--- src/infra/firebase/firebaseConfig.ts
+++ src/infra/firebase/firebaseConfig.ts
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBWveynfNg9WQOiy9bAZkE3YUojva7q_kM',
-  authDomain: 'fordev-ccd2a.firebaseapp.com',
-  projectId: 'fordev-ccd2a',
-  storageBucket: 'fordev-ccd2a.appspot.com',
-  messagingSenderId: '111139295973',
-  appId: '1:111139295973:web:fd8c0aa60f7517f18dba0c',
-  measurementId: 'G-LN8HPEDB2R'
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   mode: 'development',
@@ -51,6 +52,7 @@ module.exports = {
     },
   },
   plugins:[
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new Dotenv()
   ]
 }
