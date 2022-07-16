e1e73fbe7fa2143ce3069f6202fbcde52ae46018
Walany Costa <walanyBnegro@gmail.com>
2022-07-16 17:40:14 +0100

    refactor: move SUT creation to a factory method

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -4,15 +4,32 @@ import { CreateUserSpy } from '@/data/test';
 import { RemoteAddAccount } from './remote-add-account';
 import { StatusCode } from '@/data/protocols/firebase';
 import { EmailInUseError } from '@/domain/errors/email-in-use-error';
+import { AddAccountParam } from '@/domain/usecases';
+
+type SutType ={
+  sut: RemoteAddAccount,
+  createUserSpy: CreateUserSpy<AccountModel>,
+  addAccountParam: AddAccountParam
+};
+
+const makeSut = ():SutType => {
+  const createUserSpy = new CreateUserSpy<AccountModel>();
+  const addAccountParam = mockAddAccount();
+  createUserSpy.response = {
+    statusCode: StatusCode.ok
+  };
+  const sut = new RemoteAddAccount(createUserSpy);
+
+  return {
+    sut,
+    createUserSpy,
+    addAccountParam
+  };
+};
 
 describe('RemoteAddAccount', () => {
   test('should call CreateUser with correct params', async () => {
-    const createUserSpy = new CreateUserSpy<AccountModel>();
-    const addAccountParam = mockAddAccount();
-    createUserSpy.response = {
-      statusCode: StatusCode.ok
-    };
-    const sut = new RemoteAddAccount(createUserSpy);
+    const { sut, createUserSpy, addAccountParam } = makeSut();
     await sut.add(addAccountParam);
     expect(createUserSpy.param).toEqual({
       email: addAccountParam.email,
@@ -22,12 +39,10 @@ describe('RemoteAddAccount', () => {
   });
 
   test('should throw EmailInUseError AddAccounts returns createUser/user-exists', async () => {
-    const createUserSpy = new CreateUserSpy<AccountModel>();
+    const { sut, createUserSpy, addAccountParam } = makeSut();
     createUserSpy.response = {
       statusCode: StatusCode.forbidden
     };
-    const addAccountParam = mockAddAccount();
-    const sut = new RemoteAddAccount(createUserSpy);
     const promise = sut.add(addAccountParam);
     await expect(promise).rejects.toThrow(new EmailInUseError());
   });
