c0d1a87ed656a779813896bc158d403d6a237bc7
Walany Costa <walanyBnegro@gmail.com>
2022-07-17 14:49:56 +0100

    test: ensure SignUp returns the correct statusCode and body

--- src/infra/firebase/sign-up/sign-up.spec.ts
+++ src/infra/firebase/sign-up/sign-up.spec.ts
@@ -2,6 +2,8 @@ import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 
 import { mockAddAccount } from '@/domain/test';
 import { SignUp } from './signUp';
+import faker from '@faker-js/faker';
+import { Response } from '@/data/protocols/firebase';
 
 jest.mock('firebase/auth', () => {
   return {
@@ -10,9 +12,18 @@ jest.mock('firebase/auth', () => {
   };
 });
 
+const mockedResult = (object): Response<any> => ({
+  statusCode: 200,
+  body: object
+});
+
 describe('SignUp', () => {
   test('should calls createUserWithEmailAndPassword with correct values', async () => {
     const mockedCreateUser = createUserWithEmailAndPassword as jest.Mock<any, any>;
+    const object = jest.fn(() => faker.random.alphaNumeric());
+    mockedCreateUser.mockResolvedValue({
+      user: object
+    });
     const sut = new SignUp();
     const addAccountParam = mockAddAccount();
     await sut.signUp(addAccountParam);
@@ -20,5 +31,16 @@ describe('SignUp', () => {
       getAuth(),
       addAccountParam.email,
       addAccountParam.passwordConfirmation);
+  });
+
+  test('should return the correct statusCode and body', async () => {
+    const mockedCreateUser = createUserWithEmailAndPassword as jest.Mock<any, any>;
+    const object = jest.fn(() => faker.random.alphaNumeric());
+    mockedCreateUser.mockResolvedValue({
+      user: object
+    });
+    const sut = new SignUp();
+    const promise = sut.signUp(mockAddAccount());
+    expect(promise).toEqual(Promise.resolve(mockedResult(object)));
   });
 });
