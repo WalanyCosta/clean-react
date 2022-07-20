16aa5bd3feff301cbe5acc3aaa03ee763f5a96b8
Walany Costa <walanyBnegro@gmail.com>
2022-07-20 11:46:52 +0100

    refactor: move SUT creation a factory method

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -2,22 +2,20 @@ import { InvalidFieldError } from '@/validation/errors';
 import faker from '@faker-js/faker';
 import { CompareFIeldValidation } from './compare-fields-validation';
 
+const makeSut = (valueToCompare): CompareFIeldValidation => {
+  return new CompareFIeldValidation(faker.database.column(), valueToCompare);
+};
+
 describe('CompareFieldValidation', () => {
   test('should return error if compare is invalid', () => {
-    const sut = new CompareFIeldValidation(
-      faker.database.column(),
-      faker.random.word()
-    );
+    const sut = makeSut(faker.random.word());
     const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError());
   });
 
   test('should return falsy if compare is valid', () => {
     const valueToCompare = faker.random.word();
-    const sut = new CompareFIeldValidation(
-      faker.database.column(),
-      valueToCompare
-    );
+    const sut = makeSut(valueToCompare);
     const error = sut.validate(valueToCompare);
     expect(error).toBeFalsy();
   });
