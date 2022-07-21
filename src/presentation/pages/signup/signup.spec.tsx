e05bc227e370bd49dc77b2f89087d740ca8a76c4
Walany Costa <walanyBnegro@gmail.com>
2022-07-21 12:11:26 +0100

    test: ensure SIgnUp change email status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,16 +1,25 @@
 
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react';
 import SignUp from './signup';
-import { helper } from '@/presentation/test';
+import { helper, ValidationStub } from '@/presentation/test';
+import faker from '@faker-js/faker';
 
 type SutTypes = {
     sut: RenderResult;
 }
 
-const makeSut = () : SutTypes => {
+type SutParams = {
+  validationError?: string;
+}
+
+const makeSut = (params?: SutParams) : SutTypes => {
+  const validationStub = new ValidationStub();
+  validationStub.errorMessage = params?.validationError;
   const sut = render(
-        <SignUp />
+        <SignUp
+          validation = {validationStub}
+        />
   );
 
   return {
@@ -18,14 +27,32 @@ const makeSut = () : SutTypes => {
   };
 };
 
+const populateField = (sut: RenderResult, field:string, value = faker.random.word()): void => {
+  const input = sut.getByTestId(field);
+  fireEvent.input(input, {
+    target: {
+      value: value
+    }
+  });
+};
+
 describe('SignUp Component', () => {
+  afterEach(cleanup);
+
   test('Should start with initial state', () => {
     const validationError = 'campo obrigatório';
-    const { sut } = makeSut();
+    const { sut } = makeSut({ validationError });
     helper.testChildCount(sut, 'errorWrap', 0);
     helper.testButtonDisable(sut, 'submit');
     helper.testStatusForField(sut, 'email', validationError);
     helper.testStatusForField(sut, 'password', validationError);
     helper.testStatusForField(sut, 'passwordConfirmation', validationError);
+  });
+
+  test('should show email error if validation fails', () => {
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
+    populateField(sut, 'email');
+    helper.testStatusForField(sut, 'email', validationError);
   });
 });
