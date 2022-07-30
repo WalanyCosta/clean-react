5aa2e5bc1466b95a3b74c0ea380342682a1145ff
Walany Costa <walanyBnegro@gmail.com>
2022-07-30 13:39:49 +0100

    refactor: remove axios in the project

--- src/main/factories/usecases/authentication/remote-authentication.ts
+++
@@ -1,8 +0,0 @@
-import { RemoteAuthentication } from '@/data/usecases/authentication';
-import { Authentication } from '@/domain/usecases';
-import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory';
-
-export const makeRemoteAuthentication = (): Authentication => {
-  const url = 'http://fordevs.herokuapp.com/api/login';
-  return new RemoteAuthentication(url, makeAxiosHttpClient());
-};
