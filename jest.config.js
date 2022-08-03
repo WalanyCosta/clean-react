47d7a614977a48f3e316e3bdf4b9b8c7f60cd18f
Walany Costa <walanyBnegro@gmail.com>
2022-08-03 12:46:14 +0100

    fix: solve error in jest

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,25 @@
-module.exports ={
-    roots: ['<rootDir>/src'],
-    collectCoverageFrom:[
-        '<rootDir>/src/**/*.{ts,tsx}',
-        '!<rootDir>/src/main/**/*',
-        '!<rootDir>/src/presentation/components/router/**/*',
-        '!**/*.d.ts'
-    ],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
-    transform: {
-        '.+\\.(ts|tsx)$': 'ts-jest'
-    },
-    moduleNameMapper: {
-        '@/(.*)': '<rootDir>/src/$1',
-        '\\.scss$': 'identity-obj-proxy'
-    }
-}
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom:[
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/domain/model/**/*',
+    '!<rootDir>/src/domain/usecases/**/*',
+    '!<rootDir>/src/presentation/protocols/**/*',
+    '!<rootDir>/src/validation/protocols/**/*',
+    '!<rootDir>/src/data/protocols/cache/**/*',
+    '!<rootDir>/src/presentation/components/router/**/*',
+    '!**/*.d.ts'
+  ],
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
+};
