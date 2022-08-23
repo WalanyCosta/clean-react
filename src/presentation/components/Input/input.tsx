ff50356e3aab11574e59717c083f09d78fa15ded
Walany Costa <walanyBnegro@gmail.com>
2022-08-23 11:19:07 +0100

    refactor: add input animations

--- src/presentation/components/Input/input.tsx
+++ src/presentation/components/Input/input.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { Context } from '@/presentation/context/form/form-context';
 import Styles from './input-styles.scss';
 
@@ -7,27 +7,29 @@ type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>
 const Input: React.FC<Props> = (props: Props) => {
   const { state, setState } = useContext(Context);
   const error = state[`${props.name}Error`];
+  const inputRef = useRef<HTMLInputElement>();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>):void => {
-    setState({
-      ...state,
-      [event.target.name]: event.target.value
-    });
-  };
-
-  const getTitle = (): string => {
-    return error || 'tudo certo';
-  };
-
-  const getStatus = (): string => {
-    return error ? '🔴' : '🟢';
+    setState({ ...state, [event.target.name]: event.target.value });
   };
 
   return (
     <div className={Styles.inputWrap}>
-      <input data-testid={props.name} {...props} onChange={handleChange}/>
-      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>
-        {getStatus()}
+      <input
+        {...props}
+        ref={inputRef}
+        placeholder=" "
+        data-testid={props.name}
+        onChange={handleChange}
+      />
+      <label onClick={() => { inputRef.current.focus(); }}>
+        {props.placeholder}
+      </label>
+      <span
+        data-testid={`${props.name}-status`}
+        title={error || 'tudo certo'}
+        className={Styles.status}>
+        {error ? '🔴' : '🟢'}
       </span>
     </div>
   );
