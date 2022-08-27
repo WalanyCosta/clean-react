9189a3c802826aa2d342260c1a24d8a7f6491a92
Walany Costa <walanyBnegro@gmail.com>
2022-08-27 20:58:46 +0100

    test: ensure Signup present error state if form is invalid

--- src/main/test/nightwatch/spec/signup.js
+++ src/main/test/nightwatch/spec/signup.js
@@ -1,3 +1,4 @@
+const { faker } = require('@faker-js/faker');
 const helper = require('../page-object/form-helper')
 
 describe('Login', function () {
@@ -10,6 +11,17 @@ describe('Login', function () {
     helper.testInputStatus(signup, 'email', 'Campo obrigatório');
     helper.testInputStatus(signup, 'password', 'Campo obrigatório');
     helper.testInputStatus(signup, 'password', 'Campo obrigatório');
+    signup.getTestById('submit').to.have.attribute('disabled');
+  });
+
+  it('should present error state if form is invalid', function (browser) {
+    const signup = browser.page.object();
+    signup.setInput('email', faker.random.word());
+    helper.testInputStatus(signup, 'email', 'valor inválido');
+    signup.setInput('password', faker.random.alphaNumeric(3));
+    helper.testInputStatus(signup, 'password', 'valor inválido');
+    signup.setInput('passwordConfirmation', faker.random.alphaNumeric(4));
+    helper.testInputStatus(signup, 'passwordConfirmation', 'valor inválido');
     signup.getTestById('submit').to.have.attribute('disabled');
   });
 });
