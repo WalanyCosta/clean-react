3aeaaf69eb2a0e088d152f5d9659f727825e492d
Walany Costa <walanyBnegro@gmail.com>
2022-08-29 23:53:54 +0100

    chore: release 1.4.0

--- src/infra/firebase/authentication/auth-with-email-and-password.ts
+++ src/infra/firebase/authentication/auth-with-email-and-password.ts
@@ -7,7 +7,6 @@ export class AuthWithEmailAndPassword implements AuthFirebase<any> {
   async authFirebase (param: AuthenticationParams): Promise<Response<any>> {
     try {
       const response = await signInWithEmailAndPassword(getAuth(app), param.email, param.password);
-      console.log(response.user.uid);
       return {
         statusCode: 200,
         body: {
