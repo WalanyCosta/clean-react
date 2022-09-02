f5c44ebc00344a7d0aad46458d4efc5190456c68
Walany Costa <walanyBnegro@gmail.com>
2022-09-02 14:46:37 +0100

    test: ensure RemoteLoadSurveyList throws unexpectedError in the case error

--- src/data/protocols/firebase/get-database.ts
+++ src/data/protocols/firebase/get-database.ts
@@ -1,7 +1,9 @@
+import { Response } from './response';
+
 export type GetDatabaseParam ={
   url: string
 }
 
-export interface GetDatabase{
-  get(param: GetDatabaseParam): Promise<void>;
+export interface GetDatabase<R = any>{
+  get(param: GetDatabaseParam): Promise<Response<R>>;
 }
--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,12 +1,18 @@
-import { GetDatabase } from '@/data/protocols/firebase';
+import { GetDatabase, StatusCode } from '@/data/protocols/firebase';
+import { UnexpectedError } from '@/domain/errors';
+import { SurveyModel } from '@/domain/model';
 
 export class RemoteLoadSurveyList {
   constructor (
     private readonly url: string,
-    private readonly getDatabase: GetDatabase
+    private readonly getDatabase: GetDatabase<SurveyModel[]>
   ) {}
 
   async loadAll (): Promise<void> {
-    await this.getDatabase.get({ url: this.url });
+    const response = await this.getDatabase.get({ url: this.url });
+    switch (response.statusCode) {
+      case StatusCode.ok: break;
+      default: throw new UnexpectedError();
+    };
   }
 }
