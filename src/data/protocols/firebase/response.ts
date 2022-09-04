c2b2e37ad9b75f8842aea0369f6c46827be2ec0c
Walany Costa <walanyBnegro@gmail.com>
2022-09-04 12:48:23 +0100

    feat: ensure RemoteLoadSurveyList returns an empty list on 204

--- src/data/protocols/firebase/response.ts
+++ src/data/protocols/firebase/response.ts
@@ -3,6 +3,7 @@ export enum StatusCode {
   unauthorized = 'auth/wrong-password',
   unauthorizedUser = 'auth/user-not-found',
   serverError = 500,
+  noContent = 204,
   forbidden = 'auth/email-already-in-use'
 }
 
--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,7 +1,7 @@
 import { GetDatabase, StatusCode } from '@/data/protocols/firebase';
 import { UnexpectedError } from '@/domain/errors';
 import { SurveyModel } from '@/domain/model';
-import { LoadSurveyList } from './../../../domain/usecases/load-survey-list';
+import { LoadSurveyList } from '@/domain/usecases/load-survey-list';
 
 export class RemoteLoadSurveyList implements LoadSurveyList {
   constructor (
@@ -13,6 +13,7 @@ export class RemoteLoadSurveyList implements LoadSurveyList {
     const response = await this.getDatabase.get({ url: this.url });
     switch (response.statusCode) {
       case StatusCode.ok: return response.body;
+      case StatusCode.noContent: return [];
       default: throw new UnexpectedError();
     };
   }
