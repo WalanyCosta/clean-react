7b8c1c80cff0edce534a2d9c65229e23a39c7e85
Walany Costa <walanyBnegro@gmail.com>
2022-09-04 13:30:22 +0100

    chore: add in the configuration firebase Realtime Database

--- src/infra/firebase/firebaseConfig.ts
+++ src/infra/firebase/firebaseConfig.ts
@@ -4,6 +4,7 @@ const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
+  databaseURL: process.env.DATABASEURL,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
