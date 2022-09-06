0e7305c28561077b8961cf6f5107d36cf61abc74
Walany Costa <walanyBnegro@gmail.com>
2022-09-06 16:26:27 +0100

    feat: ensure GetDatabaseFirebase returns status 500 if get returns error

--- src/infra/firebase/get-database/get-database-firebase.ts
+++ src/infra/firebase/get-database/get-database-firebase.ts
@@ -3,11 +3,16 @@ import { get, ref, getDatabase, DataSnapshot, child } from 'firebase/database';
 
 export class GetDatabaseFirebase implements GetDatabase<any> {
   async get (param: GetDatabaseParam): Promise<any> {
-    const response = await get(child(ref(getDatabase()), param.url));
-
-    return {
-      statusCode: 200,
-      body: response.val()
-    };
+    try {
+      const response = await get(child(ref(getDatabase()), param.url));
+      return {
+        statusCode: 200,
+        body: response.val()
+      };
+    } catch (error) {
+      return {
+        statusCode: 500
+      };
+    }
   };
 }
