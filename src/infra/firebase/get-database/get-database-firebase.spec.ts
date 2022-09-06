bdb6aaf01088f607e1884f52f609901813dc5368
Walany Costa <walanyBnegro@gmail.com>
2022-09-06 13:48:12 +0100

    refactor: move SUT creation to a factory method

--- src/infra/firebase/get-database/get-database-firebase.spec.ts
+++ src/infra/firebase/get-database/get-database-firebase.spec.ts
@@ -2,6 +2,11 @@ import faker from '@faker-js/faker';
 import { getDatabase, ref, get, child } from 'firebase/database';
 import { GetDatabaseFirebase } from './get-database-firebase';
 
+type SutTypes = {
+  sut: GetDatabaseFirebase;
+  mockedGetDatabase: jest.Mock<any, any>;
+}
+
 jest.mock('firebase/database', () => {
   return {
     getDatabase: jest.fn(),
@@ -11,11 +16,20 @@ jest.mock('firebase/database', () => {
   };
 });
 
+const makeSut = (): SutTypes => {
+  const mockedGetDatabase = get as jest.Mock<any, any>;
+  const sut = new GetDatabaseFirebase();
+
+  return {
+    sut,
+    mockedGetDatabase
+  };
+};
+
 describe('GetDatabaseFirebase', () => {
   test('should calls get with value correct', async () => {
-    const mockedGetDatabase = get as jest.Mock<any, any>;
+    const { sut, mockedGetDatabase } = makeSut();
     const url = faker.internet.url();
-    const sut = new GetDatabaseFirebase();
     await sut.get({ url });
     expect(mockedGetDatabase).toHaveBeenCalledWith(
       child(ref(getDatabase()), url)
