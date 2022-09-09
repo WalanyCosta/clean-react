6b609be9463b620297efe542016924328af0e5f1
Walany Costa <walanyBnegro@gmail.com>
2022-09-09 13:00:45 +0100

    refactor: move header to its own file

--- src/presentation/components/index.tsx
+++ src/presentation/components/index.tsx
@@ -1,7 +1,8 @@
 export { default as Footer } from './footer/footer';
 export { default as FormStatus } from './form-status/form-status';
 export { default as Input } from './Input/input';
-export { default as Header } from './login-header/login-header';
+export { default as HeaderLogin } from './login-header/login-header';
 export { default as Spinner } from './spinner/spinners';
 export { default as Router } from './router/router';
 export { default as SubmitButton } from './submit-button/submit-button';
+export { default as Header } from './header/header';
