71540d7fb70b4b0a8ede90c4b275a1ad163e7db9
Walany Costa <walanyBnegro@gmail.com>
2022-09-29 15:27:57 +0100

    test: ensure SurveyItem load icon and day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -21,4 +21,17 @@ describe('SurveyItem Component', () => {
     expect(screen.getByTestId('month')).toHaveTextContent('set');
     expect(screen.getByTestId('year')).toHaveTextContent('2022');
   });
+
+  test('should render with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: false,
+      date: new Date('2021-02-02T00:00:00')
+    });
+    makeSut(survey);
+    expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown);
+    expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
+    expect(screen.getByTestId('day')).toHaveTextContent('02');
+    expect(screen.getByTestId('month')).toHaveTextContent('fev');
+    expect(screen.getByTestId('year')).toHaveTextContent('2021');
+  });
 });
