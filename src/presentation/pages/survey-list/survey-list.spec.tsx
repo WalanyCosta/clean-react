fdf5d33a2f93f1d15c3c6c7f2201c37aeb64582f
Walany Costa <walanyBnegro@gmail.com>
2022-09-29 16:18:33 +0100

    test: ensure SurveyList render SurveyItems on success

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list';
 import SurveyList from './survey-list';
 import { LoadSurveyListSpy } from '@/presentation/test/mock-load-survey-list';
@@ -18,14 +18,22 @@ const makeSut = (): SutTypes => {
 };
 
 describe('SurveyList', () => {
-  test('should present 4 empty items on start', () => {
+  test('should present 4 empty items on start', async () => {
     makeSut();
     const surveyList = screen.getByTestId('survey-list');
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
   });
 
-  test('should call LoadSurveyList', () => {
+  test('should call LoadSurveyList', async () => {
     const { loadSurveyListSpy } = makeSut();
     expect(loadSurveyListSpy.callsCount).toBe(1);
+  });
+
+  test('should render SurveyItems on success', async () => {
+    makeSut();
+    const surveyList = screen.getByTestId('survey-list');
+    waitFor(() => {
+      expect(surveyList.querySelectorAll('li:surveyItemWrap')).toHaveLength(3);
+    });
   });
 });
--- src/presentation/test/mock-load-survey-list.ts
+++ src/presentation/test/mock-load-survey-list.ts
@@ -1,10 +1,13 @@
 import { SurveyModel } from '@/domain/model';
+import { mockSurveyList } from '@/domain/test';
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list';
 
 export class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0;
+  surveys = mockSurveyList();
+
   async loadAll (): Promise<SurveyModel[]> {
     this.callsCount++;
-    return [];
+    return this.surveys;
   }
 }
