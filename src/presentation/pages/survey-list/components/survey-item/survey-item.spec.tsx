d806ab65717352677da06011456186a5890e8581
Walany Costa <walanyBnegro@gmail.com>
2022-09-29 15:06:02 +0100

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -4,12 +4,17 @@ import SurveyItem from './survey-item';
 import { mockSurveyModel } from '@/domain/test';
 import { IconName } from '@/presentation/components';
 
+const makeSut = (survey = mockSurveyModel()): void => {
+  render(<SurveyItem survey={survey}/>);
+};
+
 describe('SurveyItem Component', () => {
   test('should render with correct values', () => {
-    const survey = mockSurveyModel();
-    survey.didAnswer = true;
-    survey.date = new Date('2022-09-27T00:00:00');
-    render(<SurveyItem survey={survey}/>);
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: true,
+      date: new Date('2022-09-27T00:00:00')
+    });
+    makeSut(survey);
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp);
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
     expect(screen.getByTestId('day')).toHaveTextContent('27');
