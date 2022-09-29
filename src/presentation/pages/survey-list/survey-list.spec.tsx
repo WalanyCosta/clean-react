f9e117ab23cc8564dfea286823cbdb8922e4fd29
Walany Costa <walanyBnegro@gmail.com>
2022-09-29 17:10:12 +0100

    test: ensure SurveyList render error no failure

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -3,15 +3,14 @@ import { render, screen, waitFor } from '@testing-library/react';
 import { LoadSurveyList } from '@/domain/usecases/load-survey-list';
 import SurveyList from './survey-list';
 import { LoadSurveyListSpy } from '@/presentation/test/mock-load-survey-list';
+import { UnexpectedError } from '@/domain/errors';
 
 type SutTypes = {
   loadSurveyListSpy: LoadSurveyListSpy
 }
 
-const makeSut = (): SutTypes => {
-  const loadSurveyListSpy = new LoadSurveyListSpy();
+const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
   render(<SurveyList loadSurveyList={loadSurveyListSpy}/>);
-
   return {
     loadSurveyListSpy
   };
@@ -22,11 +21,13 @@ describe('SurveyList', () => {
     makeSut();
     const surveyList = screen.getByTestId('survey-list');
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
   });
 
   test('should call LoadSurveyList', async () => {
     const { loadSurveyListSpy } = makeSut();
     expect(loadSurveyListSpy.callsCount).toBe(1);
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument();
   });
 
   test('should render SurveyItems on success', async () => {
@@ -34,6 +35,18 @@ describe('SurveyList', () => {
     const surveyList = screen.getByTestId('survey-list');
     waitFor(() => {
       expect(surveyList.querySelectorAll('li:surveyItemWrap')).toHaveLength(3);
+      expect(screen.queryByTestId('error')).not.toBeInTheDocument();
+    });
+  });
+
+  test('should render error on failure', async () => {
+    const loadSurveyListSpy = new LoadSurveyListSpy();
+    const error = new UnexpectedError();
+    jest.spyOn(loadSurveyListSpy, 'loadAll').mockRejectedValueOnce(error);
+    makeSut(loadSurveyListSpy);
+    waitFor(() => {
+      expect(screen.queryByTestId('survey-list')).not.toBeInTheDocument();
+      expect(screen.getByTestId('error')).toHaveTextContent(error.message);
     });
   });
 });
