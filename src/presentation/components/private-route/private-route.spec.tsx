5d072a8bf174bce0276467f2dedd0769b1267ad8
Walany Costa <walanyBnegro@gmail.com>
2022-10-05 19:58:51 +0100

    refactor: move SUT creation to a factory method

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 import { ApiContext } from '@/presentation/context';
 import { render, screen } from '@testing-library/react';
 import PrivateRoute from './private-route';
-import { CurrentAccountAdapterMock } from '@/presentation/test/current-account-adapter-mock';
 import { createMemoryHistory } from 'history';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 
-describe('PrivateRoute', () => {
-  test('should redirect to / if localStorage has value account', () => {
-    const currentAccountAdapterMock = new CurrentAccountAdapterMock();
-    const setCurrentAccountMock = jest.fn();
-    render(
+const makeSut = (account = null): void => {
+  const setCurrentAccountMock = jest.fn();
+  render(
       <ApiContext.Provider value={{
         setCurrentAccount: setCurrentAccountMock,
-        getCurrentAccount: currentAccountAdapterMock.getCurrentAccountAdapter
+        getCurrentAccount: () => account
       }}>
       <HistoryRouter history={createMemoryHistory()}>
        <PrivateRoute />
       </HistoryRouter>
       </ApiContext.Provider>
-    );
+  );
+};
 
+describe('PrivateRoute', () => {
+  test('should redirect to / if localStorage has value account', () => {
+    const account = {
+      accessToken: 'laldadajdaldald',
+      email: 'delcio@gmail.com'
+    };
+    makeSut(account);
     expect(screen.getByTestId('surveyList')).toBeTruthy();
   });
 });
