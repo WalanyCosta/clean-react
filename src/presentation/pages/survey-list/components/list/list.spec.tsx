a4dbc0226bfb8de7aa502b5a0d3aabefff2d8346
Walany Costa <walanyBnegro@gmail.com>
2022-10-06 16:59:33 +0100

    test: ensure List render SurveyItem if have values on surveys

--- src/presentation/pages/survey-list/components/list/list.spec.tsx
+++ src/presentation/pages/survey-list/components/list/list.spec.tsx
@@ -1,14 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import { SurveyContext } from '@/presentation/pages/survey-list/components';
 import List from './list';
+import { mockSurveyList } from '@/domain/test';
 
-const makeSut = (surveys = []) : void => {
-  const state = { surveys };
+const makeSut = (surveysArray = []) : void => {
+  const state = { surveys: surveysArray };
   render(
-    <SurveyContext.Provider value={{ state }}>
-      <List />
-    </SurveyContext.Provider>
+      <SurveyContext.Provider value={{ state }}>
+        <List />
+      </SurveyContext.Provider>
   );
 };
 
@@ -17,5 +18,13 @@ describe('List Component', () => {
     makeSut();
     const surveyList = screen.getByTestId('survey-list');
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
+  });
+
+  test('should render SurveyItems if have values on surveys', async () => {
+    makeSut(mockSurveyList());
+    const surveyList = screen.getByTestId('survey-list');
+    waitFor(() => {
+      expect(surveyList.querySelectorAll('li:surveyItemWrap')).toHaveLength(3);
+    });
   });
 });
