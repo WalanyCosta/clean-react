c867a7233f5c586bb66b2f0ab41ab1ea00883e5f
Walany Costa <walanyBnegro@gmail.com>
2022-10-06 16:41:39 +0100

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-list/components/list/list.spec.tsx
+++ src/presentation/pages/survey-list/components/list/list.spec.tsx
@@ -1,15 +1,20 @@
 import { render, screen } from '@testing-library/react';
-import React, { useState } from 'react';
-import List from './list';
+import React from 'react';
 import { SurveyContext } from '@/presentation/pages/survey-list/components';
+import List from './list';
+
+const makeSut = (surveys = []) : void => {
+  const state = { surveys };
+  render(
+    <SurveyContext.Provider value={{ state }}>
+      <List />
+    </SurveyContext.Provider>
+  );
+};
 
 describe('List Component', () => {
   test('should render 4 items empty if not value on surveys', () => {
-    render(
-      <SurveyContext.Provider value={{ state: { surveys: [] } }}>
-        <List />
-      </SurveyContext.Provider>
-    );
+    makeSut();
     const surveyList = screen.getByTestId('survey-list');
     expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4);
   });
