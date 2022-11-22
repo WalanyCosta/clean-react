69a847042014c95fa7d91e0a339461e13f1264eb
Walany Costa <walanyBnegro@gmail.com>
2022-11-22 18:26:40 +0000

    test: ensure RemoteLoadSurveyResult calls GetDatabase with correct URL

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -0,0 +1,27 @@
+import { GetDatabaseSpy } from '@/data/test';
+import faker from '@faker-js/faker';
+import { RemoteLoadSurveyResult } from './remote-load-survey-result';
+
+type SutTypes = {
+  sut: RemoteLoadSurveyResult,
+  getDatabaseSpy: GetDatabaseSpy
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const getDatabaseSpy = new GetDatabaseSpy();
+  const sut = new RemoteLoadSurveyResult(getDatabaseSpy, url);
+  return {
+    sut,
+    getDatabaseSpy
+  };
+};
+
+describe('RemoteLoadSurveyResult', () => {
+  test('should call GetDatabase with correct URL', async () => {
+    const url = faker.internet.url();
+    const getDatabaseSpy = new GetDatabaseSpy();
+    const sut = new RemoteLoadSurveyResult(getDatabaseSpy, url);
+    await sut.load();
+    expect(getDatabaseSpy.url).toBe(url);
+  });
+});
