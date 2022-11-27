4ae5b19b5d7a6d7c88f49b37881a1d5c10f1660d
Walany Costa <walanyBnegro@gmail.com>
2022-11-27 18:47:58 +0000

    refactor: move Loading to its own file

--- src/presentation/components/index.tsx
+++ src/presentation/components/index.tsx
@@ -5,4 +5,5 @@ export { default as HeaderLogin } from './login-header/login-header';
 export { default as Spinner } from './spinner/spinners';
 export { default as SubmitButton } from './submit-button/submit-button';
 export { default as Header } from './header/header';
+export { default as Loading } from './loading/loading';
 export { default as Icon, IconName } from './icon/icon';
--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Footer, Header, Spinner } from '@/presentation/components';
+import { Footer, Header, Loading } from '@/presentation/components';
 import FlipMove from 'react-flip-move';
 import Styles from './survey-result-styles.scss';
 
@@ -27,12 +27,7 @@ const SurveyResult: React.FC = () => {
           </li>
         </FlipMove>
         <button>Voltar</button>
-        <div className={Styles.loadingWrap}>
-          <div className={Styles.loading}>
-            <span>Aguarde...</span>
-            <Spinner isNegative/>
-          </div>
-        </div>
+        {false && <Loading />}
       </div>
       <Footer />
     </div>
