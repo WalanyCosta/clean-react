4966b5bec2890b4f8e15e06fff7190fc741654dd
Walany Costa <walanyBnegro@gmail.com>
2022-11-29 19:31:54 +0000

    fix: change Spinner to remove DOM warning

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -30,7 +30,7 @@ const SurveyResult: React.FC = () => {
           </li>
         </FlipMove>
         <button>Voltar</button>
-        {false && <Loading />}
+        {true && <Loading />}
       </div>
       <Footer />
     </div>
