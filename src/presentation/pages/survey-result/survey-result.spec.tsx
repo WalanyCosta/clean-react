c608c10a5c345bda9da0b4b56c96b8fdb05ddf79
Walany Costa <walanyBnegro@gmail.com>
2022-12-05 17:59:53 +0000

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -7,17 +7,21 @@ import { createMemoryHistory } from 'history';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 
 describe('SurveyResult Component', () => {
-  test('should present correct initial state', () => {
+  const makeSut = () : void => {
     render(
-        <ApiContext.Provider value={{
-          setCurrentAccount: jest.fn(),
-          getCurrentAccount: () => mockAccount()
-        }}>
-          <HistoryRouter history={createMemoryHistory()}>
-          <SurveyResult />
-          </HistoryRouter>
-        </ApiContext.Provider>
+      <ApiContext.Provider value={{
+        setCurrentAccount: jest.fn(),
+        getCurrentAccount: () => mockAccount()
+      }}>
+        <HistoryRouter history={createMemoryHistory()}>
+        <SurveyResult />
+        </HistoryRouter>
+      </ApiContext.Provider>
     );
+  };
+
+  test('should present correct initial state', () => {
+    makeSut();
     const surveyResult = screen.getByTestId('survey-result');
     expect(surveyResult.childElementCount).toBe(0);
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
