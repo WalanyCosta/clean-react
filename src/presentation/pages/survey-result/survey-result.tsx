6bd6b9e62038cb1f82654fa2297d318c745f9959
Walany Costa <walanyBnegro@gmail.com>
2022-12-05 17:21:56 +0000

    feat: ensure SurveyResult presents correct initial state

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Calendar, Footer, Header, Loading } from '@/presentation/components';
+import React, { useState } from 'react';
+import { Calendar, Error, Footer, Header, Loading } from '@/presentation/components';
 import FlipMove from 'react-flip-move';
 import Styles from './survey-result-styles.scss';
+import { SurveyModel } from '@/domain/model';
 
 const SurveyResult: React.FC = () => {
+  const [state] = useState({
+    error: '',
+    isLoading: false,
+    surveyResult: null as SurveyModel
+  });
+
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
-      <div className={Styles.contentWrap}>
-        {true &&
+      <div data-testid='survey-result' className={Styles.contentWrap}>
+        { state.surveyResult &&
           <>
             <hgroup>
               <Calendar date={new Date()} className={Styles.calendarWrap} />
@@ -34,7 +41,8 @@ const SurveyResult: React.FC = () => {
             <button>Voltar</button>
           </>
         }
-        {false && <Loading />}
+        {state.isLoading && <Loading />}
+        {state.error && <Error error={state.error} reload={() => {}} />}
       </div>
       <Footer />
     </div>
