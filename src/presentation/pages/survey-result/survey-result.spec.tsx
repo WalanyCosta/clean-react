45ee4c2401a2aac3d8fab5c76a911ff3f2da1aaa
Walany Costa <walanyBnegro@gmail.com>
2022-12-06 20:47:55 +0000

    test: ensure SurveyResult present error if LoadSurveyResult fails

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -6,15 +6,13 @@ import { mockAccount } from '@/domain/test';
 import { createMemoryHistory } from 'history';
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom';
 import { LoadSurveyResultSpy, mockSurveyResultModel } from '@/presentation/test';
+import { UnexpectedError } from '@/domain/errors';
 
 type SutTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy
 }
 
-const makeSut = (surveyResult = mockSurveyResultModel()) : SutTypes => {
-  const loadSurveyResultSpy = new LoadSurveyResultSpy();
-  loadSurveyResultSpy.surveyResult = surveyResult;
-
+const makeSut = (loadSurveyResultSpy = new LoadSurveyResultSpy()) : SutTypes => {
   render(
     <ApiContext.Provider value={{
       setCurrentAccount: jest.fn(),
@@ -47,10 +45,12 @@ describe('SurveyResult Component', () => {
   });
 
   test('should present SurveyResult data on success', async () => {
+    const loadSurveyResultSpy = new LoadSurveyResultSpy();
     const surveyResult = Object.assign(mockSurveyResultModel(), {
       date: new Date('2022-09-27T00:00:00')
     });
-    makeSut(surveyResult);
+    loadSurveyResultSpy.surveyResult = surveyResult;
+    makeSut(loadSurveyResultSpy);
     await waitFor(() => {
       expect(screen.getByTestId('day')).toHaveTextContent('27');
       expect(screen.getByTestId('month')).toHaveTextContent('set');
@@ -70,6 +70,18 @@ describe('SurveyResult Component', () => {
       const percents = screen.queryAllByTestId('percent');
       expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`);
       expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`);
+    });
+  });
+
+  test('should render error on UnexpectedError', async () => {
+    const loadSurveyResultSpy = new LoadSurveyResultSpy();
+    const error = new UnexpectedError();
+    jest.spyOn(loadSurveyResultSpy, 'load').mockRejectedValueOnce(error);
+    makeSut(loadSurveyResultSpy);
+    await waitFor(() => {
+      expect(screen.queryByTestId('question')).not.toBeInTheDocument();
+      expect(screen.getByTestId('error')).toHaveTextContent(error.message);
+      expect(screen.queryByTestId('loading')).not.toBeInTheDocument();
     });
   });
 });
